e9c21937da151c70bc74acff4ab6434c70001a6a
Maxim Tarasyants <macartoor@gmail.com>
2020-05-21 15:55:54 +0300

    Initial commit

---
+++ src/main/java/AppData.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppData {
+
+    private String[] header;
+    private int[][] data;
+
+  public AppData (String name){
+    this.readCsvFile(name);
+  }
+   public void readCsvFile (String name){
+      List<String[]> dataList = new ArrayList<String[]>();
+      try (BufferedReader reader = new BufferedReader(new FileReader(name))) {
+        
+
+
+      }catch (IOException e) {
+          e.printStackTrace();
+      }
+   }
+}
---
+++ src/main/java/com/geekbrains/maxta/nio/MainApp.java
@@ -0,0 +1,7 @@
+package com.geekbrains.maxta.nio;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+    }
+}
